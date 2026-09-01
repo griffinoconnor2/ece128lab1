b584c3e0f5ba76419048bb8d5bb19079fef3414e
Xavier Beasley <xab228@lehigh.edu>
2026-09-01 13:49:30 -0400

    Updated Behavioral

--- 8to1muxBehavioral.v
+++ 8to1muxBehavioral.v
@@ -1,5 +1,5 @@
 module muxB(
-    input  [3:0] I [7:0], //8 Busses, 4 bits wide each
+    input  [31:0] I, //8 Busses, 4 bits wide each
     input [2:0] S, //3 Selector bits
     output reg [3:0] Y //4 bit wide output
 );
@@ -8,14 +8,14 @@ always @(*) //Always block for behavioral
     begin
         case(S)
             //Assignment for each case
-            3'b000 : Y = I[0];
-            3'b001 : Y = I[1];
-            3'b010 : Y = I[2];
-            3'b011 : Y = I[3];
-            3'b100 : Y = I[4];
-            3'b101 : Y = I[5];
-            3'b110 : Y = I[6];
-            3'b111 : Y = I[7];
+            3'b000 : Y = I[3:0];
+            3'b001 : Y = I[7:4];
+            3'b010 : Y = I[11:8];
+            3'b011 : Y = I[15:12];
+            3'b100 : Y = I[19:16];
+            3'b101 : Y = I[23:20];
+            3'b110 : Y = I[27:24];
+            3'b111 : Y = I[31:28];
             default : Y = 4'bx;
         endcase
     end
---
+++ 8to1muxDataflow.v
@@ -0,0 +1,7 @@
+module muxDF(
+    input  [3:0] I [7:0], //8 Busses, 4 bits wide each
+    input [2:0] S, //3 Selector bits
+    output [3:0] Y //4 bit wide output
+);
+
+endmodule
