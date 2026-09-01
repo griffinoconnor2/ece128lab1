a02ce41a68b5389912c758157a58f0b0662b1a76
Griffin O'Connor <griffinjoconnor@gmail.com>
2026-09-01 13:56:34 -0400

    new

--- 8to1muxStructural.v
+++ 8to1muxStructural.v
@@ -5,11 +5,24 @@ input [3:0] I0, [3:0] I1, [3:0] I2, [3:0] I3, [3:0] I4,[3:0] I5,[3:0] I6, [3:0] 
 input [2:0] S;
 output [3:0] Y;
 
-//Declare Intermediate Signals (Inversions, Ands)
+//Declare Intermediate Signals (Inversions, And's)
 wire nS0, nS1, nS2;
 wire W0, W1, W2, W3, W4, W5, W6, W7;
 
 //Perform Inversion
+not n0(nS0, S[0]);
+not n1(nS1, S[1]);
+not n2(nS2, S[1]);
+
+//And's to group Intermediate Signals According to Boolean Equation from Truth Table (in prelab)
+and a0(W0, nS2, nS1, nS0, I0);
+and a1(W1, nS2, nS1, S0, I1);
+and a2(W0, nS2, S1, nS0, I2);
+and a0(W0, nS2, nS1, nS0, I0);
+and a0(W0, nS2, nS1, nS0, I0);
+and a0(W0, nS2, nS1, nS0, I5);
+and a0(W0, nS2, nS1, nS0, I6);
+and a0(W0, nS2, nS1, nS0, I7);
 
 
 endmodule
