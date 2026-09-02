048b5bbed9ae8903050b6907f268a94473ba8ba8
Griffin O'Connor <griffinjoconnor@gmail.com>
2026-09-02 14:09:31 -0400

    outlined testbench

---
+++ 8to1muxDataflow_tb.v
@@ -0,0 +1,15 @@
+`timescale 1ns/1ps
+
+module muxDF_tb;
+
+reg [3:0] I0, I1, I2, I3, I4, I5, I6, I7, I8;
+reg [2:0] S;
+
+wire Y;
+
+//Instantiate Dataflow Mux
+muxDF uut(.I0(I0), .I1(I1), .I2(I2), .I3(I3), .I4(I4), .I5(I5), .I6(I6), .I7(I7), .S(S), .Y(Y));
+
+
+
+endmodule
