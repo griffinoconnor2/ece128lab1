c42fcdfea222563c3155880971de142c84b2375e
Xavier Beasley <xab228@lehigh.edu>
2026-09-04 15:25:18 -0400

    new

--- 8to1muxDataflow_tb.v
+++ 8to1muxDataflow_tb.v
@@ -13,7 +13,6 @@ muxDF uut(.I0(I0), .I1(I1), .I2(I2), .I3(I3), .I4(I4), .I5(I5), .I6(I6), .I7(I7)
 initial 
     begin 
         //Initial Values at t=0
-        //Each I signal corresponds to its bus number for ease of testing 
         S  = 3'b000;
         I0 = 1'b1; 
         I1 = 1'b0;
