9117a0fa16e73f4b71108e52934a860c084ca6ad
Xavier Beasley <xab228@lehigh.edu>
2026-09-04 13:48:38 -0400

    new

--- 8to1muxBehavioral.v
+++ 8to1muxBehavioral.v
@@ -1,7 +1,7 @@
 module muxB(
-    input  [3:0] I0, I1, I2, I3, I4, I5, I6, I7,//8 Busses, 4 bits wide each
+    input I0, I1, I2, I3, I4, I5, I6, I7,//8 inputs
     input [2:0] S, //3 Selector bits
-    output reg [3:0] Y //4 bit wide output
+    output reg Y
 );
 
 always @(*) //Always block for behavioral
@@ -16,7 +16,7 @@ always @(*) //Always block for behavioral
             3'b101 : Y = I5;
             3'b110 : Y = I6;
             3'b111 : Y = I7;
-            default : Y = 4'bx;
+            default : Y = 1'bx;
         endcase
     end
 
