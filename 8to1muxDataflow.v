0f5a164b1a523b7973f86772d2dc0a7123dfdf31
Xavier Beasley <xab228@lehigh.edu>
2026-09-04 13:50:49 -0400

    new

--- 8to1muxDataflow.v
+++ 8to1muxDataflow.v
@@ -1,20 +1,18 @@
 module muxDF(
-    input  [3:0] I0, I1, I2, I3, I4, I5, I6, I7,//8 Busses, 4 bits wide each
+    input  I0, I1, I2, I3, I4, I5, I6, I7,//8 inputs
     input [2:0] S, //3 Selector bits
-    output [3:0] Y //4 bit wide output
+    output Y //Output
 );
 
-//Please not that I used the replication operator {4{...}} to expand the 1 bit selector into 4 bit so that the bitwise operations would work properly. 
-//Readme will have the source I used linked
 assign Y = (
-            ({4{~S[2] & ~S[1] & ~S[0]}} & I0) | 
-            ({4{~S[2] & ~S[1] &  S[0]}} & I1) |
-            ({4{~S[2] &  S[1] & ~S[0]}} & I2) |
-            ({4{~S[2] &  S[1] &  S[0]}} & I3) | 
-            ({4{ S[2] & ~S[1] & ~S[0]}} & I4) | 
-            ({4{ S[2] & ~S[1] &  S[0]}} & I5) |
-            ({4{ S[2] &  S[1] & ~S[0]}} & I6) |
-            ({4{ S[2] &  S[1] &  S[0]}} & I7)
+            (~S[2] & ~S[1] & ~S[0] & I0) | 
+            (~S[2] & ~S[1] &  S[0] & I1) |
+            (~S[2] &  S[1] & ~S[0] & I2) |
+            (~S[2] &  S[1] &  S[0] & I3) | 
+            ( S[2] & ~S[1] & ~S[0] & I4) | 
+            ( S[2] & ~S[1] &  S[0] & I5) |
+            ( S[2] &  S[1] & ~S[0] & I6) |
+            ( S[2] &  S[1] &  S[0] & I7)
             );
 
 endmodule
